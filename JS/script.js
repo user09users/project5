71190cb847e7e0c9f0d329f765e1dd9614146f05
Mkh <example@gmail.com>
2025-01-02 20:35:15 +0100

    some changes

--- JS/script.js
+++ JS/script.js
@@ -1,45 +1,30 @@
-'use strict';
-
-let numberOfFilms;
-function start(){
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-    whille(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)); {
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    }
-
-}
-start();
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red',
+    },
+    makeTest: function() {
+        console.log('Test');
+    },
 };
+options.makeTest();
+console.log(Object.keys(optoins).length);
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените его?', '');
+for (key in options) {
+    if (typeof(options[key]) === 'object') {
+        for (let i in options[key])
+            console.log(`key is ${i} and value is ${options[key][i]}`);
+    } else { console.log(`key is ${key } and value is ${options[key]}`);
 
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
     }
-}
+};
+console.log()
 
-if (personalMovieDB.count < 10) {
-    console.log("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("Вы классический зритель");
-} else if (personalMovieDB.count >= 30) {
-    console.log("Вы киноман");
-} else {
-    console.log("Произошла ошибка");
-}
 
-console.log(personalMovieDB);
+
+const str = prompt('','');
+const products = str.split(', ');
+console.log(products);
