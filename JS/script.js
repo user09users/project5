34aa54eb146746d647ccdf726687f25d48611ddc
Mkh <example@gmail.com>
2025-01-12 12:36:57 +0100

    somechanges

--- JS/script.js
+++ JS/script.js
@@ -1,29 +1,5 @@
-const shops = [
-{rice : 500},
-{oil : 100},
-{bread : 50},
-];
- const budget = [1000, 5000, 10000];
- 
- const map = new Map([ 
-   // [{paper: 400}, 7000]
- ]);
-
- //shops.forEach((shop, i) => {
-   //map.set(shop, budget[i])
- //});
-console.log(map);
-
-
-
-const goods = [];
- for (let shop of map.keys()) {
-    goods.push(Object.keys(shop)[0])
+function deepCount(a){
+let count = a.length;
+for(i=0; i< a; i ++) if( Array.isArray( a[i]) ) {count += deepCount(a[i])};
+return count;
 };
-console.log(goods);
-
-
-
-for (let [shop, price] of map.entries) {
-    console.log(price, shop)
-}
