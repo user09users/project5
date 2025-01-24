3852c487344d327bff8cde6cad947dd8a7f4aa44
Mkh <example@gmail.com>
2025-01-24 16:03:55 +0100

    something

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,19 @@
-function deepCount(a){
-let count = a.length;
-for(i=0; i< a; i ++) if( Array.isArray( a[i]) ) {count += deepCount(a[i])};
-return count;
-};
+/* 
+const compose = (...fns) => (x) => fns.reduceRight((res, fn) => fn(res), x);
+
+const add1 = x => x + 1;
+const multiply2 = x => x * 2;
+
+const f = compose(multiply2, add1);
+console.log(f(3)); // (3 + 1) * 2 = 8
+ */
+
+const composeWithArgs = (...fns) => fns.reduceRight((f, g) => (...args) => g(f(...args)));
+
+
+
+const add = (x, y) => x + y;
+const multiply = (x, y) => x * y;
+
+const f = composeWithArgs(multiply, add);
+console.log(f(2, 3)); // (2 + 3) * 2 = 10
